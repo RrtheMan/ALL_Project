3c5015d52f484251b24afaa87ab896ce984f108b
RrtheMan <124282736+RrtheMan@users.noreply.github.com>
2025-01-19 16:17:19 +0000

    Delete Public_html directory

--- Public_html/Public_html/functions/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-ob_start();
-session_start();
-
-require_once('functions/db.php');
-require_once('functions/functions.php');
-
-?>
--- Public_html/Public_html/functions/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once('functions/db.php');
-require_once('functions/config.php');
-
-?>
